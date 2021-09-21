26e7d4e5c582e19f2a70f83fa4a984af12c11c3a
felipealp <felipealvesalp@gmail.com>
2021-09-21 11:05:09 -0300

    feat navbar component

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,33 @@
 <template>
   <div id="app">
-    <router-view />
+    <Main />
   </div>
 </template>
 
 <script>
+import Main from "@/pages/Main";
+
 export default {
   name: "App",
+
+  components: {
+    Main,
+  },
 };
 </script>
 
-<style>
+<style lang="scss">
+html,
+body {
+  margin: 0px !important;
+  padding: 0px !important;
+}
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  height: 100vh;
+  background: #a49bb6;
 }
 </style>
---
+++ src/components/Navbar/index.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="navbar">
+    <div class="logo">    
+      <img src="https://www.ticto.com.br/dist/assets/logo-branca.svg" alt="Logotipo Boosthub" />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Navbar",
+};
+</script>
+
+<style>
+.navbar {
+  width: 100%;
+  height: 60px;
+  background: #151020;
+}
+
+.logo {
+  width: 150px;
+  padding: 10px 2rem;
+}
+</style>
