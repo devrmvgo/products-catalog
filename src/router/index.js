d21dc93570aef2d83a142c77947781d45a76d473
felipealp <felipealvesalp@gmail.com>
2021-09-21 10:14:56 -0300

    started products list routes

---
+++ src/router/index.js
@@ -0,0 +1,22 @@
+import Vue from "vue";
+import VueRouter from "vue-router";
+
+import Products from "@/pages/Products";
+
+Vue.use(VueRouter);
+
+const routes = [
+  {
+    path: "/",
+    name: "Products",
+    component: Products,
+  },
+];
+
+const router = new VueRouter({
+  mode: "history",
+  base: process.env.BASE_URL,
+  routes,
+});
+
+export default router;
